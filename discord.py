6008d32e6699aedded0f8ee0a69db53fe244513e
speed-max-1234 <godwilliop@gmail.com>
2026-02-27 23:31:12 +0530

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,1 +1,11 @@
+import discord
+import os
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot ready")
+
+bot.run(os.getenv("srv-d6gtfdua2pns7386d4e0?key=yZT1qF1wUww"))
