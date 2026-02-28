7b070cd18467f0f019b3cda7ff4a60da100ec900
speed-max-1234 <godwilliop@gmail.com>
2026-02-28 08:58:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
-import discord
 import os
+os.environ["DISCORD_NO_VOICE"] = "1"
+
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print("Bot ready")
+    print(f"Logged in as {bot.user}")
 
 bot.run(os.getenv("srv-d6gtfdua2pns7386d4e0?key=yZT1qF1wUww"))
